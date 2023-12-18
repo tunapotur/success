8c7890962d7f9f033b092290b4e4e8ffa2b0d8ca
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-12-18 15:45:33 +0300

    first design

--- app/page.js
+++ app/page.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main>First Commit</main>;
+  return (
+    <main className="border-red-500 border sm:max-w-[40rem] min-h-screen basis-full">
+      First Commit
+    </main>
+  );
 }
