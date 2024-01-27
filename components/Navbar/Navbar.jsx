f39afbc1efd5715a4a52b4f735a5af801002a916
Ahmet Tuna POTUR <tpotur@gmail.com>
2024-01-27 18:03:05 +0300

    navbar code repeats is fixed

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -2,81 +2,54 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useSession, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 import { SlLogin } from "react-icons/sl";
 import { FaRegSquarePlus } from "react-icons/fa6";
 import { FaUser } from "react-icons/fa";
 import GoalSkelaton from "../GoalSkelatonSvg";
 
-const iconSize = "h-[2.5rem] w-[2.5rem]";
-
 function Navbar() {
   const { status } = useSession();
   const pathname = usePathname();
 
+  //Navbar Icons
+  const Login = <NavbarIcon link={"login"} Icon={SlLogin} />;
+  const UserProfile = <NavbarIcon link={"userprofile/1"} Icon={FaUser} />;
+  const SuccessLogo = (
+    <NavbarIcon link={"/"} Icon={GoalSkelaton} label={"Success"} />
+  );
+  const AddSuccess = (
+    <NavbarIcon
+      link={status === "unauthenticated" ? "login" : "addsuccess"}
+      Icon={FaRegSquarePlus}
+    />
+  );
+
   return (
     <nav className="flex h-full flex-row items-center justify-around">
-      {pathname === "/" || pathname === "/successdetail" ? (
+      {pathname === "/" || pathname.includes("/success/") ? (
         <>
-          {status === "unauthenticated" ? <Login /> : <UserProfile />}
-          <SuccessLogo />
-          <AddSuccess />
+          {status === "unauthenticated" ? Login : UserProfile}
+          {SuccessLogo}
+          {AddSuccess}
         </>
       ) : (
-        <SuccessLogo />
+        <>{SuccessLogo}</>
       )}
     </nav>
   );
 }
 
-// TODO: Logo fonksiyonları için fonksiyon döndüren bir pattern vardı onunla kod tekrarını düzelt
-
-function SuccessLogo() {
+function NavbarIcon({ link, Icon, label }) {
   const { status } = useSession();
 
   if (status === "loading") return <div>loading...</div>;
 
   return (
-    <Link className="flex flex-col items-center justify-center" href={"/"}>
-      <GoalSkelaton className={`${iconSize} fill-black dark:fill-white`} />
-      <div className="text-[0.75rem] leading-none">Success</div>
-    </Link>
-  );
-}
-
-function Login() {
-  const { status } = useSession();
-
-  if (status === "loading") return <div>loading...</div>;
-
-  return (
-    <Link href={"/login"}>
-      <SlLogin className={`${iconSize}`} />
-    </Link>
-  );
-}
-
-function UserProfile() {
-  const { status } = useSession();
-
-  if (status === "loading") return <div>loading...</div>;
-
-  return (
-    <Link href={"/userprofile/1"}>
-      <FaUser className={`${iconSize}`} />
-    </Link>
-  );
-}
-
-function AddSuccess({ linkUrl }) {
-  const { status } = useSession();
-
-  if (status === "loading") return <>loading...</>;
-
-  return (
-    <Link href={status === "unauthenticated" ? "login" : "addsuccess"}>
-      <FaRegSquarePlus className={`${iconSize}`} />
+    <Link className="flex flex-col items-center justify-center" href={link}>
+      <Icon className={"h-[2.5rem] w-[2.5rem]"} />
+      {label && <div className="text-[0.75rem] leading-none">{label}</div>}
     </Link>
   );
 }
