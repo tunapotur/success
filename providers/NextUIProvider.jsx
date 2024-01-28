1b0c3a02b4992e5e1233fc61157e16384a7cd5ae
Ahmet Tuna POTUR <tpotur@gmail.com>
2024-01-28 18:07:52 +0300

    Nextui routing

--- providers/NextUIProvider.jsx
+++ providers/NextUIProvider.jsx
@@ -1,7 +1,16 @@
 "use client";
 
 import { NextUIProvider as NextUI } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+
+/*
+* Nextui Routing
+I added nextui routing specification. Nextui mention it will be useful for cliend side router.
+https://nextui.org/docs/guide/routing
+*/
 
 export const NextUIProvider = ({ children }) => {
-  return <NextUI>{children}</NextUI>;
+  const router = useRouter();
+
+  return <NextUI navigate={router.push}>{children}</NextUI>;
 };
