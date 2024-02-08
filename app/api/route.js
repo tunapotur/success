aeaecf96edb2c4935392a63351b11e469436fc37
Ahmet Tuna POTUR <tpotur@gmail.com>
2024-02-08 21:25:20 +0300

    route

--- app/api/route.js
+++ app/api/route.js
@@ -1,18 +1,13 @@
 import { NextResponse, NextRequest } from "next/server";
-import { useSession, getSession } from "next-auth/react";
-import { getProviders } from "next-auth/react";
-
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export async function GET(req) {
-  const session = await getSession();
-  const providers = await getProviders();
+  const session = await getServerSession(authOptions);
 
   return NextResponse.json({
     time: new Date().toLocaleString(),
     CurrentUser: "Yeah",
     Session: session,
-    Provider: providers,
   });
 }
