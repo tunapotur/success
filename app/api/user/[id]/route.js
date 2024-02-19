686c302953864760a9ddaf386bbbaac37b957d87
Ahmet Tuna POTUR <tpotur@gmail.com>
2024-02-19 15:39:25 +0300

    get user api

--- app/api/user/[id]/route.js
+++ app/api/user/[id]/route.js
@@ -3,6 +3,29 @@ import { connectMongoDB } from "@/lib/mongodb";
 import User from "@/models/User";
 import getServerSessionInfo from "@/lib/getServerSessionInfo";
 
+export async function GET(req) {
+  try {
+    const session = await getServerSessionInfo();
+
+    if (!session) throw new Error("Unauthorized user. Please login or signup.");
+
+    await connectMongoDB();
+
+    const userId = session?.user.id;
+
+    const user = await User.findById(userId);
+
+    return NextResponse.json({ user }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: `An error occurred while updating the user profile. ${error.message}`,
+      },
+      { status: 500 },
+    );
+  }
+}
+
 export async function PUT(req) {
   try {
     const session = await getServerSessionInfo();
