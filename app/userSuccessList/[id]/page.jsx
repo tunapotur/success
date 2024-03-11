fd7a4ce9d61df4e6a82354cecaca21f7581290d9
Ahmet Tuna POTUR <tpotur@gmail.com>
2024-03-11 15:39:54 +0300

    folder move and one todo

--- app/userSuccessList/[id]/page.jsx
+++ app/userSuccessList/[id]/page.jsx
@@ -1,12 +1,7 @@
 // TODO: bune bakılacak export const dynamicParams = true;
-/* TODO: bu sayfa her kullanıcı tarafından açılabilir olacak.
- *  Succesler üzerinde kullanıcıya tıklanınca kullanıcının tüm succesleri açılacak
- *  Tüm succeslerde bir de kullanıcı profilini görebileceğimiz bir link olacak*/
-/* TODO: Kullanıcı profili sayfası tasarla. K. Adi, e-mail'i, profil fotosu gibi bilgiler olacak*/
-// TODO: bu ekranda unauthenticated oldunduğunda navbar main page ikonu görünmüyor
-// TODO: navbar main page ikonu koşul olmadan tüm sayfalarda görünsün.
+// TODO: Succesler üzerinde kullanıcıya tıklanınca kullanıcının tüm succesleri açılacak
+// TODO: Tüm succeslerde bir de kullanıcı profilini görebileceğimiz bir link olacak*/
 // TODO: olmayan bir kulanıcı id'si url param olarak girilirse not found page dönecek
-// TODO: next: { revalidate: 0 }, parametresi nextjs'in fetch i cash'lemesi. bunun için const dosyasına belirli değerler girip tanımlanacak.
 
 import SuccessCart from "@/components/SuccessCart";
 import { SUCCESS_LIST_REVALIDATE_DURATION } from "@/data/constants";
