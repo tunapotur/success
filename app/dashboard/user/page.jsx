b5ea045338c82dd82b09e25fde09345d4e73b888
Ahmet Tuna POTUR <tpotur@gmail.com>
2024-04-12 17:44:45 +0300

    build

--- app/dashboard/user/page.jsx
+++ app/dashboard/user/page.jsx
@@ -1,7 +1,9 @@
 function User() {
-  return     <main className="main">
-  <h1 className="page-name">User Dashboard</h1>
-</main>;
+  return (
+    <main className="main">
+      <h1 className="page-name">User Dashboard</h1>
+    </main>
+  );
 }
 
 export default User;
